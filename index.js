eb36283543d56176c6ad6fa8ce012d43a18883c8
timatrue <rollmaxidom@gmail.com>
2019-07-16 23:21:42 +0300

    server logging

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  console.log('income');
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
@@ -15,3 +16,18 @@ server.listen(port, hostname, () => {
 });
 
 //https://stackoverflow.com/questions/23281895/node-js-eacces-error-when-listening-on-http-80-port-permission-denied
+//https://serverfault.com/questions/288729/once-i-set-iptables-to-reroute-a-port-how-do-i-undo-it
+/*
+http://84.201.159.199/
+sudo iptables -t nat -D PREROUTING -i eth0 -p tcp --dport 22 -j REDIRECT --to-port 3000
+iptables -t nat -D PREROUTING -p tcp --dport 22 -j REDIRECT --to 3000
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 3000
+
+sudo iptables -S
+
+sudo iptables -L -n -t nat
+
+https://stackoverflow.com/questions/49237886/nodejs-app-wont-create-server-says-err-connection-timed-out
+https://gist.github.com/tomasevich/a2fe588c451c5a192893e6521a813020
+https://stackoverflow.com/questions/5009324/node-js-nginx-what-now
+*/
