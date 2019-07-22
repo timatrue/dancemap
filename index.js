00cb7d5dc95a0f79ed47ed8530c732f96e841685
Artem Trushinskiy <rollmaxidom@gmail.com>
2019-07-22 16:19:22 +0300

    redirect to https

--- index.js
+++ index.js
@@ -22,16 +22,22 @@ const certificate = fs.readFileSync('/etc/letsencrypt/live/dancemap.online/cert.
 const ca = fs.readFileSync('/etc/letsencrypt/live/dancemap.online/chain.pem', 'utf8');
 
 const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
+    key: privateKey,
+    cert: certificate,
+    ca: ca
 };
 
-
-
 app.use(express.static(__dirname + '/static', { dotfiles: 'allow' } ))
+
 app.get('/', function (req, res) {
-   res.send('dancemap here');
+    //res.send('dancemap here');
+    if(req.secure){
+     // OK, continue
+     return next();
+   };
+   // handle port numbers if you need non defaults
+   // res.redirect('https://' + req.host + req.url); // express 3.x
+   res.redirect('https://' + req.hostname + req.url); // express 4.x
 })
 
 // Starting both http & https servers
