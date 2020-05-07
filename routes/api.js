02d0137d18774faed8062647516b9ac50c4326d2
timatrue <rollmaxidom@gmail.com>
2020-05-07 07:13:55 +0300

    sitemap generate events,studios,root dirs

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Studio = require('../models/studio');
+const Event = require('../models/event');
+const path = require('path')
+const { buildSitemaps } = require('express-sitemap-xml')
+const fs = require('fs')
 
 router.get('/mongo', function(req, res) {
 	Studio.find({}).then(function(studio) { 
@@ -13,9 +17,55 @@ router.get('/mongo', function(req, res) {
     Studio.countDocuments({}).then(function(err, count) {
       console.log('GET', count);
     });
+})
 
+router.get('/generatesitemap', function(req, res) {
+  Promise.all([getStudioList(), getEventsList()])
+    .then((values) => {
+      const rootUrls = getRootList();
+      const studioUrls = values[0];
+      const eventUrls = values[1];
+      let urls = [...rootUrls,...studioUrls, ...eventUrls];
+      console.log('generatesitemapall', urls);
+      const sitemaps = buildSitemaps(urls,'https://dancemap.online')
+      return sitemaps;
+    })
+    .then((sitemaps) => {
+        const sitemap = sitemaps["/sitemap.xml"]
+        fs.writeFileSync(path.join(__dirname, '../static/sitemap.xml'), sitemap.toString())
+        console.log('sitemap', sitemap)
+        res.send(sitemap)
+    });
 
 })
+
+getStudioList = () => {
+  return Studio.distinct("_id", {})
+    .then(studios => {
+      const urls = studios.map(studio => { 
+        return {url:`/studios/${studio}`, changeFreq: 'monthly', lastMod: new Date('2020-01-01')}
+      })
+      return urls;
+    })
+}
+
+getEventsList = () => {
+  return Event.distinct("_id", {})
+    .then(events => {
+      const urls = events.map(event => { 
+        return {url:`/events/${event}`, changeFreq: 'monthly', lastMod: new Date('2020-01-01')}
+      })
+      return urls;
+    })
+}
+
+getRootList = () => {
+  let urls = []; 
+  urls.push({url:'/studios', changeFreq: 'monthly', lastMod: new Date('2020-01-01')})
+  urls.push({url:'/events', changeFreq: 'monthly', lastMod: new Date('2020-01-01')})
+  return urls;
+}
+
 
 router.get('/studios/:id', function(req, res) {
     console.log('GET /studio', req.params);  
@@ -48,5 +98,9 @@ router.put('/mongo/:id', function(req, res) {
 router.delete('/mongo/:id', function(req, res) {
 	res.send({type:'DELETE'});
 });
+
+
+
+
 
 module.exports = router;
--- routes/studios.js
+++ routes/studios.js
@@ -2,11 +2,6 @@ const express = require('express')
 const router = express.Router()
 const Studio = require('../models/studio')
 const path = require('path')
-const { buildSitemaps } = require('express-sitemap-xml')
-const fs = require('fs')
-
-
-
 
 router.get('/:id', function(req, res) {
     console.log('GET /studio', req.params);  
@@ -28,25 +23,4 @@ router.get('/:id', function(req, res) {
     }
 })
 
-router.get('/:id/generatesitemap', function(req, res) {
-     Studio.distinct("_id", {})
-      .then(studios => {
-        const urls = studios.map(studio => { 
-        	return {url:`/studios/${studio}`, changeFreq: 'monthly', lastMod: new Date('2020-01-01')}
-        })
-        return urls; })
-      .then(urls => {
-        urls.push({url:'/studios', changeFreq: 'monthly', lastMod: new Date('2020-01-01')})
-        urls.push({url:'/events', changeFreq: 'monthly', lastMod: new Date('2020-01-01')})
-        
-        const sitemaps = buildSitemaps(urls,'https://dancemap.online')
-        return sitemaps
-      })
-      .then( sitemaps => {
-      	const sitemap = sitemaps["/sitemap.xml"]
-      	fs.writeFileSync(path.join(__dirname, '../static/sitemap.xml'), sitemap.toString())
-      	console.log('sitemap', sitemap)
-      	res.send(sitemap)
-      })
-})
 module.exports = router;
