cb8f8fbb526f89f5c9b2204ef24f3ebba17de973
timatrue <rollmaxidom@gmail.com>
2020-05-24 16:35:15 +0300

    uppy uploader connected (very basic)

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const app = express()
 const favicon = require('serve-favicon')
 const bodyParser = require("body-parser")
 const fs = require('fs')
+const formidable = require('formidable')
 /*TIME FORMAT*/
 const dayjs = require('dayjs')
 require('dayjs/locale/ru')
 dayjs.locale('ru')
+
+
+
 
 //Mongo cloud
 //const MongoClient = require('mongodb').MongoClient;
@@ -38,6 +42,42 @@ app.use(express.static(__dirname + '/static', { dotfiles: 'allow' } ))
 app.use('/api', require('./routes/api'))
 app.use('/studios', require('./routes/studios'))
 app.use('/events', require('./routes/events'))
+
+
+
+app.post('/upload', uploadRoute)
+
+function uploadRoute (req, res) {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000 // 30 days
+    /** add other headers as per requirement */
+  }
+    // parse a file upload
+    var form = new formidable.IncomingForm()
+    form.uploadDir = './static/uploads'
+    form.keepExtensions = true
+
+    form.parse(req, function (err, fields, files) {
+      if (err) {
+        console.log('some error', err)
+        res.writeHead(200, headers)
+        res.write(JSON.stringify(err))
+        return res.end()
+      }
+      var file = files['files[]']
+      console.log('saved file to', file.path)
+      console.log('original name', file.name)
+      console.log('type', file.type)
+      console.log('size', file.size)
+      res.writeHead(200, headers)
+      res.write(JSON.stringify({ fields, files }))
+      return res.end()
+    })
+}
+
 
 //Mongoose connect
 ;(async () => {
